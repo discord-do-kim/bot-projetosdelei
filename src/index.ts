6865d4c4819535b37282201a60ba71986b2f2e19
Papagaio <github@devs.kim>
2024-01-18 06:12:37 -0300

    fix: assume Client<true> on params after client is initialized

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { client } from "./client";
 import mongoose from "mongoose";
 import { eventDispatcher } from "./dispatcher/EventDispatcher";
 import projetosDeLei from "./addons/projetosDeLei";
+import { Events } from "discord.js";
 
 projetosDeLei();
 
@@ -16,6 +17,9 @@ projetosDeLei();
     });
   });
 
+  client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+  });
   await client.login(DISCORD_TOKEN);
 })().catch((e) => {
   console.log(e);
